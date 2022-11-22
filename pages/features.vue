8d687915d99a2d664311f4b881899fe507b54ebb
ohiosveryown <matt@ohiosveryown.co>
2022-11-22 15:49:31 +0000

    masonary layout

--- pages/features.vue
+++ pages/features.vue
@@ -8,149 +8,154 @@
 
     <section class="icon-wrapper">
       <ul class="icons">
-        <li class="icon">
+        <li>
           <img
             src="https://res.cloudinary.com/da32ufmnf/image/upload/v1669129127/atlas-refresh/features/yoosdxzeshd8ljparxwc.png"
             alt=""
+            class="icon"
           />
         </li>
 
-        <li class="icon">
+        <li>
           <img
             src="https://res.cloudinary.com/da32ufmnf/image/upload/v1669129127/atlas-refresh/features/i5zz7nglrnrfpss86ae8.png"
             alt=""
+            class="icon"
           />
         </li>
 
-        <li class="icon">
+        <li>
           <img
             src="https://res.cloudinary.com/da32ufmnf/image/upload/v1669129127/atlas-refresh/features/kpremdyi4m1mgxftjm1f.png"
             alt=""
+            class="icon"
           />
         </li>
 
-        <li class="icon">
+        <li>
           <img
             src="https://res.cloudinary.com/da32ufmnf/image/upload/v1669129127/atlas-refresh/features/yoosdxzeshd8ljparxwc.png"
             alt=""
+            class="icon"
           />
         </li>
 
-        <li class="icon">
+        <li>
           <img
             src="https://res.cloudinary.com/da32ufmnf/image/upload/v1669129127/atlas-refresh/features/i5zz7nglrnrfpss86ae8.png"
             alt=""
+            class="icon"
           />
         </li>
 
-        <li class="icon">
+        <li>
           <img
             src="https://res.cloudinary.com/da32ufmnf/image/upload/v1669129127/atlas-refresh/features/kpremdyi4m1mgxftjm1f.png"
             alt=""
+            class="icon"
           />
         </li>
       </ul>
 
       <ul class="icons">
-        <li class="icon">
+        <li>
           <img
             src="https://res.cloudinary.com/da32ufmnf/image/upload/v1669129127/atlas-refresh/features/yoosdxzeshd8ljparxwc.png"
             alt=""
+            class="icon"
           />
         </li>
 
-        <li class="icon">
+        <li>
           <img
             src="https://res.cloudinary.com/da32ufmnf/image/upload/v1669129127/atlas-refresh/features/i5zz7nglrnrfpss86ae8.png"
             alt=""
+            class="icon"
           />
         </li>
 
-        <li class="icon">
+        <li>
           <img
             src="https://res.cloudinary.com/da32ufmnf/image/upload/v1669129127/atlas-refresh/features/kpremdyi4m1mgxftjm1f.png"
             alt=""
+            class="icon"
           />
         </li>
 
-        <li class="icon">
+        <li>
           <img
             src="https://res.cloudinary.com/da32ufmnf/image/upload/v1669129127/atlas-refresh/features/yoosdxzeshd8ljparxwc.png"
             alt=""
+            class="icon"
           />
         </li>
 
-        <li class="icon">
+        <li>
           <img
             src="https://res.cloudinary.com/da32ufmnf/image/upload/v1669129127/atlas-refresh/features/i5zz7nglrnrfpss86ae8.png"
             alt=""
+            class="icon"
           />
         </li>
       </ul>
     </section>
 
     <main class="width">
-      <section class="features">
-        <ul>
-          <card
-            class="card"
-            icon="📼"
-            heading="pixel perfect replay"
-            subheading="A video is worth a thousand words. Let your customers show you rather
-            then tell you with session recording."
-            product="https://res.cloudinary.com/da32ufmnf/image/upload/v1668995137/atlas-refresh/index/p3k5m2qclpvpg2iqnl5w.png"
-            bg="https://res.cloudinary.com/da32ufmnf/image/upload/v1645805170/atlas/f8kyzjyzmdpcftqd0bmc.png"
-          />
+      <card
+        class="card"
+        icon="📼"
+        heading="pixel perfect replay"
+        subheading="A video is worth a thousand words."
+        product="https://res.cloudinary.com/da32ufmnf/image/upload/v1668995137/atlas-refresh/index/p3k5m2qclpvpg2iqnl5w.png"
+        bg="https://res.cloudinary.com/da32ufmnf/image/upload/v1645805170/atlas/f8kyzjyzmdpcftqd0bmc.png"
+      />
 
-          <card
-            class="card"
-            icon="📼"
-            heading="pixel perfect replay"
-            subheading="A video is worth a thousand words."
-            product="https://res.cloudinary.com/da32ufmnf/image/upload/v1668995137/atlas-refresh/index/p3k5m2qclpvpg2iqnl5w.png"
-            bg="https://res.cloudinary.com/da32ufmnf/image/upload/v1645805170/atlas/f8kyzjyzmdpcftqd0bmc.png"
-          />
+      <card
+        class="card"
+        icon="📼"
+        heading="pixel perfect replay"
+        subheading="A video is worth a thousand words. Let your customers..."
+        product="https://res.cloudinary.com/da32ufmnf/image/upload/v1668995137/atlas-refresh/index/p3k5m2qclpvpg2iqnl5w.png"
+        bg="https://res.cloudinary.com/da32ufmnf/image/upload/v1645805170/atlas/f8kyzjyzmdpcftqd0bmc.png"
+      />
 
-          <card
-            class="card"
-            icon="📼"
-            heading="pixel perfect replay"
-            subheading="A video is worth a thousand words. Let your customers."
-            product="https://res.cloudinary.com/da32ufmnf/image/upload/v1668995137/atlas-refresh/index/p3k5m2qclpvpg2iqnl5w.png"
-            bg="https://res.cloudinary.com/da32ufmnf/image/upload/v1645805170/atlas/f8kyzjyzmdpcftqd0bmc.png"
-          />
+      <card
+        class="card"
+        icon="📼"
+        heading="pixel perfect replay"
+        subheading="A video is worth a thousand words. Let your customers show you rather
+        then tell you with session recording."
+        product="https://res.cloudinary.com/da32ufmnf/image/upload/v1668995137/atlas-refresh/index/p3k5m2qclpvpg2iqnl5w.png"
+        bg="https://res.cloudinary.com/da32ufmnf/image/upload/v1645805170/atlas/f8kyzjyzmdpcftqd0bmc.png"
+      />
 
-          <card
-            class="card"
-            icon="📼"
-            heading="pixel perfect replay"
-            subheading="A video is worth a thousand words. Let your customers show you rather
-            then tell you with session recording. Show you rather
-            then tell you with session recording"
-            product="https://res.cloudinary.com/da32ufmnf/image/upload/v1668995137/atlas-refresh/index/p3k5m2qclpvpg2iqnl5w.png"
-            bg="https://res.cloudinary.com/da32ufmnf/image/upload/v1645805170/atlas/f8kyzjyzmdpcftqd0bmc.png"
-          />
+      <card
+        class="card"
+        icon="📼"
+        heading="pixel perfect replay"
+        subheading="A video is worth a thousand words."
+        product="https://res.cloudinary.com/da32ufmnf/image/upload/v1668995137/atlas-refresh/index/p3k5m2qclpvpg2iqnl5w.png"
+        bg="https://res.cloudinary.com/da32ufmnf/image/upload/v1645805170/atlas/f8kyzjyzmdpcftqd0bmc.png"
+      />
 
-          <card
-            class="card"
-            icon="📼"
-            heading="pixel perfect replay"
-            subheading="A video is worth a thousand words. Let your customers show you rather
-            then tell you with session recording."
-            product="https://res.cloudinary.com/da32ufmnf/image/upload/v1668995137/atlas-refresh/index/p3k5m2qclpvpg2iqnl5w.png"
-            bg="https://res.cloudinary.com/da32ufmnf/image/upload/v1645805170/atlas/f8kyzjyzmdpcftqd0bmc.png"
-          />
+      <card
+        class="card"
+        icon="📼"
+        heading="pixel perfect replay"
+        subheading="A video is worth a thousand words. Let your customers."
+        product="https://res.cloudinary.com/da32ufmnf/image/upload/v1668995137/atlas-refresh/index/p3k5m2qclpvpg2iqnl5w.png"
+        bg="https://res.cloudinary.com/da32ufmnf/image/upload/v1645805170/atlas/f8kyzjyzmdpcftqd0bmc.png"
+      />
 
-          <card
-            class="card"
-            icon="📼"
-            heading="pixel perfect replay"
-            subheading="A video is worth a thousand words."
-            product="https://res.cloudinary.com/da32ufmnf/image/upload/v1668995137/atlas-refresh/index/p3k5m2qclpvpg2iqnl5w.png"
-            bg="https://res.cloudinary.com/da32ufmnf/image/upload/v1645805170/atlas/f8kyzjyzmdpcftqd0bmc.png"
-          />
-        </ul>
-      </section>
+      <card
+        class="card"
+        icon="📼"
+        heading="pixel perfect replay"
+        subheading="A video is worth a thousand words. Let your customers show you rather
+        then tell you with session recording."
+        product="https://res.cloudinary.com/da32ufmnf/image/upload/v1668995137/atlas-refresh/index/p3k5m2qclpvpg2iqnl5w.png"
+        bg="https://res.cloudinary.com/da32ufmnf/image/upload/v1645805170/atlas/f8kyzjyzmdpcftqd0bmc.png"
+      />
     </main>
   </div>
 </template>
@@ -167,7 +172,26 @@
 
   main {
     position: relative;
-    padding-bottom: 50vh;
+    margin: 0 auto;
+    column-count: 1;
+    column-gap: 2.4rem;
+
+    @include breakpoint(sm) {
+      margin-top: 9.6rem;
+      column-count: 2;
+      column-gap: 2.4rem;
+    }
+
+    @include breakpoint(mdl) {
+      column-count: 3;
+      column-gap: 2.4rem;
+    }
+  }
+
+  .card {
+    break-inside: avoid;
+    margin-bottom: 3.2rem;
+    padding: 2rem;
   }
 
   section.icon-wrapper {
@@ -186,37 +210,6 @@
   .icon {
     width: 88px;
   }
-
-  section.features {
-    margin-top: 6.4rem;
-  }
-
-  section.features ul {
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-template-rows: masonry;
-  }
-
-  .card {
-    border: 1px solid red;
-  }
-
-  //   .features ul {
-  //     display: flex;
-  //     margin: 0 auto 4rem;
-  //
-  //     @include breakpoint(md) {
-  //       justify-content: space-between;
-  //       // width: grid-width(10);
-  //     }
-  //
-  //     li {
-  //       @include breakpoint(md) {
-  //         padding: 2rem;
-  //         width: grid-width(3.7);
-  //       }
-  //     }
-  //   }
 </style>
 
 <script>
